4a9e8f73049b486200c161bb6102a7c0621a3b45
Claudio <claudiovarrosi@gmail.com>
2022-06-15 23:08:52 +0200

    Lavoro completo

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="comic">
+      <img :src="info.thumb" :alt="info.series">
+      <h5>{{ info.series }}</h5>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ComicCard',
+  props: {
+    info: Object
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.comic {
+  margin: 10px;
+  width: 15%;
+  img {
+    width: 100%;
+  }
+  h5 {
+    color: white;
+  }
+}
+</style>
